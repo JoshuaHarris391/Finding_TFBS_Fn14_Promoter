ca929636af8d2294f38ec43f85cb3e6feceb47db
36280641+JoshuaHarris391@users.noreply.github.com <harjo391@gmail.com>
2019-07-25 16:59:07 +1200

    removed duplicate script

--- quality_control.sh
+++
@@ -1,7 +0,0 @@
-# Setting working directory
-cd /home/STUDENT/harjo391/JRA/JRA_5_TFBS_Fn14_Promoter
-# Loading fastqc
-module load FastQC/0.11.5-Java-1.8.0_74
-# Running fastqc on all fastq files
-mkdir -p output/fastqc_untrimmed
-fastqc ../test_data/*.fastq.gz --outdir=fastqc_untrimmed/
