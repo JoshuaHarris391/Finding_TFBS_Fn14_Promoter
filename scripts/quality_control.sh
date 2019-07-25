f9fa8cb68a45ede8ae8b5e81fe95990fe2e4df57
36280641+JoshuaHarris391@users.noreply.github.com <harjo391@gmail.com>
2019-07-25 16:56:05 +1200

    changed file structure

--- scripts/quality_control.sh
+++ scripts/quality_control.sh
@@ -3,5 +3,5 @@ cd /home/STUDENT/harjo391/JRA/JRA_5_TFBS_Fn14_Promoter
 # Loading fastqc
 module load FastQC/0.11.5-Java-1.8.0_74
 # Running fastqc on all fastq files
-mkdir -p output/fastqc_untrimmed
+mkdir -p fastqc_untrimmed
 fastqc ../test_data/*.fastq.gz --outdir=fastqc_untrimmed/
