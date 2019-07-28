b78d77797dbd0aa8ea2506ec71d89e5227bf5c8a
36280641+JoshuaHarris391@users.noreply.github.com <harjo391@gmail.com>
2019-07-28 20:35:28 +1200

    fixed server pull issues

--- scripts/server_pull_fastqc.sh
+++ scripts/server_pull_fastqc.sh
@@ -2,7 +2,7 @@
 # [LOCAL TERMINAL] Run this in local terminal to pull fastqc files
 ####################################
 scp -r STUDENT+harjo391@dsmc0.otago.ac.nz:~/JRA/JRA_5_TFBS_Fn14_Promoter/outputs/fastqc_untrimmed \
-    ~/Dropbox/Research/JRA_Cunliffe_Lab_2019/Experiments/JRA_5/JRA_5_TFBS_Fn14_Promoter/fastqc_local/
+    ~/Dropbox/Research/JRA_Cunliffe_Lab_2019/Experiments/JRA_5/JRA_5_TFBS_Fn14_Promoter/fastqc_untrimmed/
 
 scp -r STUDENT+harjo391@dsmc0.otago.ac.nz:~/JRA/JRA_5_TFBS_Fn14_Promoter/outputs/fastqc_trimmed \
-    ~/Dropbox/Research/JRA_Cunliffe_Lab_2019/Experiments/JRA_5/JRA_5_TFBS_Fn14_Promoter/fastqc_local/
+    ~/Dropbox/Research/JRA_Cunliffe_Lab_2019/Experiments/JRA_5/JRA_5_TFBS_Fn14_Promoter/fastqc_trimmed/
