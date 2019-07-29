fec07d6acf58c6100878a6d1926ba62efc93c488
joshuaharris391 <harjo391@gmail.com>
2019-07-29 15:30:13 +1200

    updated hg19 file reference for decompressed version

--- scripts/bwa_variant_calling.sh
+++ scripts/bwa_variant_calling.sh
@@ -13,6 +13,7 @@ mkdir -p  outputs/alignments/sam \
 # cd hg19
 # wget http://hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/hg19.fa.gz
 # chmod -w *.gz
+# gunzip hg19.fa.gz
 # cd /home/STUDENT/harjo391/JRA/JRA_5_TFBS_Fn14_Promoter
 
 #################################
@@ -22,4 +23,4 @@ echo "Begining BWA Alignment"
 # Indexing hg19 reference
 echo "Indexing hg19 reference"
 module load "BWA/0.7.17-foss-2018b"
-bwa index hg19/hg19.fa.gz
+bwa index hg19/hg19.fa
