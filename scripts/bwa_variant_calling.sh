555cea7137f6c67c0e23af45f4488fed90c4d2f8
36280641+JoshuaHarris391@users.noreply.github.com <harjo391@gmail.com>
2019-07-30 15:01:12 +1200

    added convert to bam

--- scripts/bwa_variant_calling.sh
+++ scripts/bwa_variant_calling.sh
@@ -20,17 +20,33 @@ mkdir -p  outputs/alignments/sam \
 #################################
 # Alignment BWA
 #################################
-echo "Begining BWA Alignment"
+echo "[UPDATE] Begining BWA Alignment"
 module load "BWA/0.7.17-foss-2018b"
 # # Indexing hg19 reference
 # echo "Indexing hg19 reference"
 # module load "BWA/0.7.17-foss-2018b"
 # bwa index hg19/hg19.fa
 
-echo "Running BWA mem aligner"
+echo "[UPDATE] Running BWA mem aligner"
 
+for BWA_FILE in outputs/fastq_trimmed/*_1.trimmed.fastq.gz; do
+base=$(basename ${BWA_FILE} _1.trimmed.fastq.gz)
 bwa mem -t 4 -P ../data/hg19/hg19.fa \
-        outputs/fastq_trimmed/SRR8652105_pass_1.trimmed.fastq.gz \
-        outputs/fastq_trimmed/SRR8652105_pass_2.trimmed.fastq.gz > \
-        outputs/alignments/sam/SRR8652105.aligned.sam
-echo "finished alignment"
+        outputs/fastq_trimmed/${base}_1.trimmed.fastq.gz \
+        outputs/fastq_trimmed/${base}_2.trimmed.fastq.gz > \
+        outputs/alignments/sam/${base}.aligned.sam
+echo "[UPDATE] completed $base alignment"
+done
+
+#################################
+# Convert to Bam
+#################################
+echo "[UPDATE] converting to bam file"
+module load 'SAMtools/1.9-foss-2016b'
+# Converting to bam
+for SAM_FILE in outputs/alignments/*.aligned.sam; do
+base=$(basename ${SAM_FILE} .aligned.sam)
+samtools view -S -b outputs/alignments/sam/$base.aligned.sam > \
+                    outputs/alignments/bam/$base.aligned.bam
+echo "[UPDATE] converted $base to bam"
+done
