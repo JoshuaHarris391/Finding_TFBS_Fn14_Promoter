16c7ec8e45a4747398f7eec1407a19262b6fd1a7
36280641+JoshuaHarris391@users.noreply.github.com <harjo391@gmail.com>
2019-08-05 19:02:27 +1200

    fixed server transfer paths

--- scripts/server_transfer.sh
+++ scripts/server_transfer.sh
@@ -6,9 +6,9 @@
 DEST=~/Dropbox/Research/JRA_Cunliffe_Lab_2019/Experiments/JRA_5/JRA_5_TFBS_Fn14_Promoter
 
 # Pulling Fastqc Files
-scp -r STUDENT+harjo391@dsmc0.otago.ac.nz:~/JRA/JRA_5_TFBS_Fn14_Promoter/outputs/fastqc_untrimmed/\* $DEST
+scp -r STUDENT+harjo391@dsmc0.otago.ac.nz:~/JRA/JRA_5_TFBS_Fn14_Promoter/outputs/fastqc_untrimmed $DEST
 
-scp -r STUDENT+harjo391@dsmc0.otago.ac.nz:~/JRA/JRA_5_TFBS_Fn14_Promoter/outputs/fastqc_trimmed/\*
+scp -r STUDENT+harjo391@dsmc0.otago.ac.nz:~/JRA/JRA_5_TFBS_Fn14_Promoter/outputs/fastqc_trimmed $DEST
 
 # # Pulling BAM files
 # scp -r STUDENT+harjo391@dsmc0.otago.ac.nz:~/JRA/JRA_5_TFBS_Fn14_Promoter/outputs/alignments/bam \
