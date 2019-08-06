9731ff0ec6f1647d8b5f74c9cf33c236fc65907b
joshuaharris391 <harjo391@gmail.com>
2019-08-06 16:52:12 +1200

    increased amount of test data reads

--- scripts/download_test_data.sh
+++ scripts/download_test_data.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=Download_SRA_Test_Data # job name (shows up in the queue)
+#SBATCH --time=10:00:00 #Walltime (HH:MM:SS)
+#SBATCH --mem=1000 # Memory in MB
+#SBATCH --cpus-per-task=1
+#SBATCH --mail-type=END
+#SBATCH --mail-user=harjo391@student.otago.ac.nz
+
 # Set wd
 cd /home/STUDENT/harjo391/JRA/JRA_5_TFBS_Fn14_Promoter
 # Download files
@@ -15,7 +23,7 @@ for SRA_REF_VAR in ${SRA_REF_LIST[@]}; do
       --split-3 \
        --clip \
        -N 10000 \
-       -X 110000 \
+       -X 1010000 \
        $SRA_REF_VAR
 done
 # check file size
