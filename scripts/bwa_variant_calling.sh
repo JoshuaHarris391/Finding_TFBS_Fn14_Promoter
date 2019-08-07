0a2507a740ee0dcd32f650e5e87a691e9068b12d
36280641+JoshuaHarris391@users.noreply.github.com <harjo391@gmail.com>
2019-08-07 13:37:24 +1200

    Made script to download hg19 reference genome

--- scripts/bwa_variant_calling.sh
+++ scripts/bwa_variant_calling.sh
@@ -17,25 +17,12 @@ mkdir -p  outputs/alignments/sam \
           outputs/variant_calls/vcf \
           outputs/variant_calls/bcf
 
-# Downloading hg19 human reference genome
-# mkdir -p ../data/hg19
-# cd ../data/hg19
-# wget http://hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/hg19.fa.gz
-# chmod -w *.gz
-# gunzip hg19.fa.gz
-# cd $WORKING_DIR
 
 #################################
 # Alignment BWA
 #################################
 echo "[UPDATE] Begining BWA Alignment"
 module load "BWA/0.7.17-foss-2018b"
-# # Indexing hg19 reference
-# echo "Indexing hg19 reference"
-# module load "BWA/0.7.17-foss-2018b"
-# bwa index hg19/hg19.fa
-
-echo "[UPDATE] Running BWA mem aligner"
 
 bwa mem -t 4 -P ../data/hg19/hg19.fa \
         outputs/fastq_trimmed/$SRA_REF/${SRA_REF}_1.trimmed.fastq.gz \
