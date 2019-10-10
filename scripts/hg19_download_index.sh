6dfaaf7d648eaa3211d182411912191b63917fc5
36280641+JoshuaHarris391@users.noreply.github.com <harjo391@gmail.com>
2019-10-10 17:25:41 +1300

    index hg19 reference with samtools faidx

--- scripts/hg19_download_index.sh
+++ scripts/hg19_download_index.sh
@@ -1,15 +1,21 @@
+# Setting modules
+module purge
+module load "SAMtools/1.9-foss-2018a"
+module load "BWA/0.7.17-foss-2018b"
 # Setting WD
 WORKING_DIR=/Users/joshua_harris/Dropbox/Research/JRA_Cunliffe_Lab_2019/Experiments/JRA_5/JRA_5_TFBS_Fn14_Promoter
 cd $WORKING_DIR
 # Downloading hg19 human reference genome
 mkdir -p ../data/hg19
 cd ../data/hg19
-wget http://hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/hg19.fa.gz
-chmod -w *.gz
-gunzip hg19.fa.gz
+# wget http://hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/hg19.fa.gz
+# chmod -w *.gz
+# gunzip hg19.fa.gz
 cd $WORKING_DIR
 
-# Indexing hg19 reference
+# Indexing hg19 reference with BWA
 echo "Indexing hg19 reference"
-# module load "BWA/0.7.17-foss-2018b"
-bwa index ../data/hg19/hg19.fa
+# bwa index ../data/hg19/hg19.fa
+
+# Indexing with SAMtools
+samtools faidx ../data/hg19/hg19.fa
