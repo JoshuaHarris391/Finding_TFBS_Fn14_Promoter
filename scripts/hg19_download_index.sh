e0e003ffeeca253fd47826f19cfd7472432d33ee
36280641+JoshuaHarris391@users.noreply.github.com <harjo391@gmail.com>
2019-10-11 15:27:43 +1300

    fixed file paths

--- scripts/hg19_download_index.sh
+++ scripts/hg19_download_index.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #SBATCH --job-name=index_hg19 # job name (shows up in the queue)
 #SBATCH --time=24:00:00 #Walltime (HH:MM:SS)
-#SBATCH --mem=6000 # Memory in MB
-#SBATCH --cpus-per-task=4
+#SBATCH --mem=4000 # Memory in MB
+#SBATCH --cpus-per-task=2
 
 # Setting modules
 module purge
 module load "SAMtools/1.9-foss-2018a"
 module load "BWA/0.7.17-foss-2018b"
 # Setting WD
-WORKING_DIR=/Users/joshua_harris/Dropbox/Research/JRA_Cunliffe_Lab_2019/Experiments/JRA_5/JRA_5_TFBS_Fn14_Promoter
+WORKING_DIR=/home/STUDENT/harjo391/JRA/JRA_5_TFBS_Fn14_Promoter
 cd $WORKING_DIR
 # Downloading hg19 human reference genome
 mkdir -p ../data/hg19
