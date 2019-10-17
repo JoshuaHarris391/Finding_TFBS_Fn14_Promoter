151f6bf817c158a7a1195bc4b4f1b9f67595b15e
36280641+JoshuaHarris391@users.noreply.github.com <harjo391@gmail.com>
2019-10-17 19:51:10 +1300

    updated mother script formatting and inactivated download GRCh37

--- scripts/mother_script.sh
+++ scripts/mother_script.sh
@@ -1,19 +1,26 @@
 # Stop script if there is an error
 set -e
+
 # Setting WD
 cd /home/STUDENT/harjo391/JRA/JRA_5_TFBS_Fn14_Promoter
+
 # Removing slurm outputs
 touch tmp.out
 rm *.out
+
 # Removing current outputs folder
 rm -R outputs
-# Downloading and indexing GRCh37
-JOB_0=$(sbatch --parsable scripts/GRCh37_download_index.sh)
+
+# # Downloading and indexing GRCh37
+# JOB_0=$(sbatch --parsable scripts/GRCh37_download_index.sh)
+
 # Getting filenames
 filenames=$(ls ../test_data/*_1.fastq.gz)
 SRA_REF=$(basename -s _1.fastq.gz $filenames)
+
 # Defining Data folder
 DATA=../test_data
+
 # Running quality control
 for filename_input in ${SRA_REF[*]}; do
   echo "== Running QC on $filename_input =="
