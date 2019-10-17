3041d995bb36c40a2bd963f9474f3dcc80ba89e1
36280641+JoshuaHarris391@users.noreply.github.com <harjo391@gmail.com>
2019-10-17 15:37:56 +1300

    fixed file path

--- scripts/validate_bam.sh
+++ scripts/validate_bam.sh
@@ -15,4 +15,4 @@ module load Java/1.10.0_1
 # Defining GATK GATK_PATH
 GATK_PATH=/home/STUDENT/harjo391/tools/GATK/gatk-4.1.3.0/gatk-package-4.1.3.0-local.jar
 # Running Validate Sam File
-java -jar $GATK_PATH ValidateSamFile -I outputs/alignments/bam/SRR8652105_pass.chr16.aligned.sorted.bam -MODE SUMMARY
+java -jar $GATK_PATH ValidateSamFile -I outputs/alignments/bam/chr16/SRR8652105_pass.chr16.aligned.sorted.bam -MODE SUMMARY
