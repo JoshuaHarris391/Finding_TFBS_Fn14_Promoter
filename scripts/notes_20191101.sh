14ad8a135b17c55cfc88439e80e1d0cf67ff7a94
36280641+JoshuaHarris391@users.noreply.github.com <harjo391@gmail.com>
2019-11-01 16:07:21 +1300

    added history notes

---
+++ scripts/notes_20191101.sh
@@ -0,0 +1,12 @@
+java -jar $GATK_PATH Mutect2 -R /resource/bundles/broad_bundle_b37_v2.5/human_g1k_v37_decoy.fasta -I test.bam --germline-resource /resource/bundles/gnomAD/vcf/genomes/gnomad.genomes.r2.0.2.sites.vcf.bgz -O out.vcf.gz
+
+# Add -R to bwa mem or use picard AddOrReplaceReadGroups
+# - Adds read groups to header of bam file
+# MarkDuplicates see if its suggested with GATK4, older versions suggest using this
+# Add this to Mutect2,
+# --disable-sequence-dictionary-validation true
+# Update file paths to absolute, and use /resource/bundles/ stuff
+
+# Run scripts on /scratch
+# Store data on HCS, transfer to scratch, run scripts, transfer output back to HCS, delete scratch
+# Transfer all samples first
