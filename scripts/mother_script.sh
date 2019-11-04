fa68a7f810b83ab1151a608729f9833ad5921d1e
36280641+JoshuaHarris391@users.noreply.github.com <harjo391@gmail.com>
2019-11-05 11:36:15 +1300

    Just run trimming and alignment scripts

--- scripts/mother_script.sh
+++ scripts/mother_script.sh
@@ -22,24 +22,24 @@ SRA_REF=$(basename -s _1.fastq.gz $filenames)
 DATA=../test_data
 
 # Running quality control
-# for filename_input in ${SRA_REF[*]}; do
-#   echo "== Running QC on $filename_input =="
-# 	# Use dependancy if running GRCh37 download
-#   JOB_1=$(sbatch --dependency=afterany:$JOB_0 --export=SRA_REF=$filename_input,DATA=$DATA --parsable scripts/quality_control.sh)
-# 	# JOB_1=$(sbatch --export=SRA_REF=$filename_input,DATA=$DATA --parsable scripts/quality_control.sh)
-# done
+for filename_input in ${SRA_REF[*]}; do
+  echo "== Running QC on $filename_input =="
+	# Use dependancy if running GRCh37 download
+  # JOB_1=$(sbatch --dependency=afterany:$JOB_0 --export=SRA_REF=$filename_input,DATA=$DATA --parsable scripts/quality_control.sh)
+	JOB_1=$(sbatch --export=SRA_REF=$filename_input,DATA=$DATA --parsable scripts/quality_control.sh)
+done
 
 # Running alignment
 for filename_input in ${SRA_REF[*]}; do
   echo "== Running alignment on $filename_input =="
-  # JOB_2=$(sbatch --dependency=afterany:$JOB_1 --export=SRA_REF=$filename_input,DATA=$DATA --parsable scripts/bwa_alignment.sh)
-	JOB_2=$(sbatch --export=SRA_REF=$filename_input,DATA=$DATA --parsable scripts/bwa_alignment.sh)
+  JOB_2=$(sbatch --dependency=afterany:$JOB_1 --export=SRA_REF=$filename_input,DATA=$DATA --parsable scripts/bwa_alignment.sh)
+	# JOB_2=$(sbatch --export=SRA_REF=$filename_input,DATA=$DATA --parsable scripts/bwa_alignment.sh)
 done
 
 # Running Mutect2
-for filename_input in ${SRA_REF[*]}; do
-  echo "== Mutect2 on $filename_input =="
-  JOB_3=$(sbatch --dependency=afterany:$JOB_2 --export=SRA_REF=$filename_input,DATA=$DATA --parsable scripts/Variant_Calling.sh)
-done
+# for filename_input in ${SRA_REF[*]}; do
+#   echo "== Mutect2 on $filename_input =="
+#   JOB_3=$(sbatch --dependency=afterany:$JOB_2 --export=SRA_REF=$filename_input,DATA=$DATA --parsable scripts/Variant_Calling.sh)
+# done
 
 echo "== END OF HEAD SCRIPT=="
