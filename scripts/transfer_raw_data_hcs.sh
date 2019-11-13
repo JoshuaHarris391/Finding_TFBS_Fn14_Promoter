994ced3f7c9c3d657b9b9d8bad7430557efb37e8
36280641+JoshuaHarris391@users.noreply.github.com <harjo391@gmail.com>
2019-11-13 14:04:18 +1300

    updated link to hcs and reduced cores

--- scripts/transfer_raw_data_hcs.sh
+++ scripts/transfer_raw_data_hcs.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=transfer_raw_data_hcs # job name (shows up in the queue)
 #SBATCH --time=24:00:00 #Walltime (HH:MM:SS)
 #SBATCH --mem=2000 # Memory in MB
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=1
 #SBATCH --output=slurm_%x_%j.out
 
 # transfering from /scratch to hcs
-scp -r STUDENT+harjo391@dsmc0.otago.ac.nz:/scratch/STUDENT+harjo391/JRA_5 /mnt/hcs/dsm-molecularoncology/Josh_Harris_Bioinformatics/
+scp -r STUDENT+harjo391@dsmc0.otago.ac.nz:/scratch/STUDENT+harjo391/JRA_5 STUDENT+harjo391@dsmc0.otago.ac.nz:/mnt/hcs/dsm-molecularoncology/Josh_Harris_Bioinformatics/
