af3b719ff4d9098cf362b2915370e7d4f9bc4af8
36280641+JoshuaHarris391@users.noreply.github.com <harjo391@gmail.com>
2019-11-15 12:29:15 +1300

    mother runs all

--- scripts/mother_script.sh
+++ scripts/mother_script.sh
@@ -13,8 +13,8 @@ cd $OUTPUT_DATA
 touch tmp.out
 rm *.out
 
-# Removing current outputs folder
-# rm -R outputs
+Removing current outputs folder
+rm -R outputs
 
 # # Downloading and indexing GRCh37
 # JOB_0=$(sbatch --parsable scripts/GRCh37_download_index.sh)
@@ -26,19 +26,19 @@ DATA=../test_data/
 filenames=$(ls $DATA/*_1.fastq.gz)
 SRA_REF=$(basename -s _1.fastq.gz $filenames)
 
-# # Running quality control
-# for filename_input in ${SRA_REF[*]}; do
-#   echo "== Running QC on $filename_input =="
-# 	# Use dependancy if running GRCh37 download
-#   # JOB_1=$(sbatch --dependency=afterany:$JOB_0 --export=SRA_REF=$filename_input,DATA=$DATA --parsable $SCRIPT_REF/quality_control.sh)
-# 	JOB_1=$(sbatch --export=SRA_REF=$filename_input,DATA=$DATA --parsable $SCRIPT_REF/quality_control.sh)
-# done
+# Running quality control
+for filename_input in ${SRA_REF[*]}; do
+  echo "== Running QC on $filename_input =="
+	# Use dependancy if running GRCh37 download
+  # JOB_1=$(sbatch --dependency=afterany:$JOB_0 --export=SRA_REF=$filename_input,DATA=$DATA --parsable $SCRIPT_REF/quality_control.sh)
+	JOB_1=$(sbatch --export=SRA_REF=$filename_input,DATA=$DATA --parsable $SCRIPT_REF/quality_control.sh)
+done
 
 # Running alignment
 for filename_input in ${SRA_REF[*]}; do
   echo "== Running alignment on $filename_input =="
-  # JOB_2=$(sbatch --dependency=afterany:$JOB_1 --export=SRA_REF=$filename_input,DATA=$DATA --parsable $SCRIPT_REF/bwa_alignment.sh)
-	JOB_2=$(sbatch --export=SRA_REF=$filename_input,DATA=$DATA --parsable $SCRIPT_REF/bwa_alignment.sh)
+  JOB_2=$(sbatch --dependency=afterany:$JOB_1 --export=SRA_REF=$filename_input,DATA=$DATA --parsable $SCRIPT_REF/bwa_alignment.sh)
+	# JOB_2=$(sbatch --export=SRA_REF=$filename_input,DATA=$DATA --parsable $SCRIPT_REF/bwa_alignment.sh)
 done
 
 # Running Mutect2
