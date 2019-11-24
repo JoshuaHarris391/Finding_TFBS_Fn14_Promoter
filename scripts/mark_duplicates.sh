4eea1cfed498cbfdf528c965ca448d6ebbdce333
36280641+JoshuaHarris391@users.noreply.github.com <harjo391@gmail.com>
2019-11-25 12:43:33 +1300

    using GATK4 markduplicates

--- scripts/mark_duplicates.sh
+++ scripts/mark_duplicates.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=Mark_Duplicates # job name (shows up in the queue)
 #SBATCH --time=72:00:00 #Walltime (HH:MM:SS)
 #SBATCH --mem=20000 # Memory in MB
-#SBATCH --cpus-per-task=3
+#SBATCH --cpus-per-task=4
 #SBATCH --output=slurm_%x_%j.out
 
 # Setting Directory variables
@@ -19,4 +19,9 @@ module purge
 
 # Marking duplicates
 module load picard
-java -jar $EBROOTPICARD/picard.jar MarkDuplicates I=outputs/alignments/bam/${SRA_REF}.aligned.sorted.bam O=outputs/alignments/bam/${SRA_REF}.dedup.aligned.sorted.bam M=outputs/alignments/bam/${SRA_REF}_MarkDuplicates.txt
+# java -jar $EBROOTPICARD/picard.jar MarkDuplicates I=outputs/alignments/bam/${SRA_REF}.aligned.sorted.bam O=outputs/alignments/bam/${SRA_REF}.dedup.aligned.sorted.bam M=outputs/alignments/bam/${SRA_REF}_MarkDuplicates.txt
+
+# Using GATK
+module load Java/1.10.0_1
+GATK_PATH=/home/STUDENT/harjo391/tools/GATK/gatk-4.1.3.0/gatk-package-4.1.3.0-local.jar
+java -jar $GATK_PATH MarkDuplicates I=outputs/alignments/bam/${SRA_REF}.aligned.sorted.bam O=outputs/alignments/bam/${SRA_REF}.dedup.aligned.sorted.bam M=outputs/alignments/bam/${SRA_REF}_MarkDuplicates.txt
