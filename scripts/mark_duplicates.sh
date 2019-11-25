ce954d164e185f357ebe76c6147359c36eb69174
36280641+JoshuaHarris391@users.noreply.github.com <harjo391@gmail.com>
2019-11-26 09:29:15 +1300

    updated resource allocation and deactivated temp var

--- scripts/mark_duplicates.sh
+++ scripts/mark_duplicates.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #SBATCH --job-name=Mark_Duplicates # job name (shows up in the queue)
-#SBATCH --time=72:00:00 #Walltime (HH:MM:SS)
-#SBATCH --mem=40000 # Memory in MB
-#SBATCH --cpus-per-task=4
+#SBATCH --time=15:00:00 #Walltime (HH:MM:SS)
+#SBATCH --mem=30000 # Memory in MB
+#SBATCH --cpus-per-task=2
 #SBATCH --output=slurm_%x_%j.out
 
 # Setting Directory variables
 SCRIPT_REF=/home/STUDENT/harjo391/JRA/JRA_5_TFBS_Fn14_Promoter/scripts
 OUTPUT_DATA=/scratch/STUDENT+harjo391/JRA_5/JRA_5_TFBS_Fn14_Promoter
 
-# Defining SRA ref temp
-SRA_REF='SRR8652105'
+# # Defining SRA ref temp
+# SRA_REF='SRR8652105'
 
 # Set wd
 set -e
