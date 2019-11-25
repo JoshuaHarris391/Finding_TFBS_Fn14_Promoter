f7e3ad7b8a32b55d5a941a13289d3db2772d98ea
36280641+JoshuaHarris391@users.noreply.github.com <harjo391@gmail.com>
2019-11-26 09:26:00 +1300

    added in references and modules

--- scripts/bam_index.sh
+++ scripts/bam_index.sh
@@ -5,6 +5,19 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --output=slurm_%x_%j.out
 
+# Setting Directory variables
+SCRIPT_REF=/home/STUDENT/harjo391/JRA/JRA_5_TFBS_Fn14_Promoter/scripts
+OUTPUT_DATA=/scratch/STUDENT+harjo391/JRA_5/JRA_5_TFBS_Fn14_Promoter
+
+# Defining SRA ref temp
+SRA_REF='SRR8652105'
+
+# Set wd
+set -e
+cd $OUTPUT_DATA
+module purge
+module load 'SAMtools'
+
 # Indexing bam files
 echo "[UPDATE] indexing bam files"
 samtools index --threads 8 outputs/alignments/bam/${SRA_REF}.dedup.aligned.sorted.bam
