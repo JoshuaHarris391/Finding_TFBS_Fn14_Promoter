37e87ab03dc5fcc3e4b8378795fbcffdb45cb190
36280641+JoshuaHarris391@users.noreply.github.com <harjo391@gmail.com>
2019-11-26 09:50:34 +1300

    updated job name

--- scripts/bam_index.sh
+++ scripts/bam_index.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=BWA_Align # job name (shows up in the queue)
+#SBATCH --job-name=bam_index # job name (shows up in the queue)
 #SBATCH --time=72:00:00 #Walltime (HH:MM:SS)
 #SBATCH --mem=40000 # Memory in MB
 #SBATCH --cpus-per-task=4
