6d05f8b920019e9146ae559cb9970876f28cf545
36280641+JoshuaHarris391@users.noreply.github.com <harjo391@gmail.com>
2019-11-26 13:26:32 +1300

    updated resource req, and inactivated temp var

--- scripts/bam_index.sh
+++ scripts/bam_index.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=bam_index # job name (shows up in the queue)
-#SBATCH --time=72:00:00 #Walltime (HH:MM:SS)
-#SBATCH --mem=40000 # Memory in MB
-#SBATCH --cpus-per-task=4
+#SBATCH --time=1:00:00 #Walltime (HH:MM:SS)
+#SBATCH --mem=2000 # Memory in MB
+#SBATCH --cpus-per-task=2
 #SBATCH --output=slurm_%x_%j.out
 
 # Setting Directory variables
@@ -10,7 +10,7 @@ SCRIPT_REF=/home/STUDENT/harjo391/JRA/JRA_5_TFBS_Fn14_Promoter/scripts
 OUTPUT_DATA=/scratch/STUDENT+harjo391/JRA_5/JRA_5_TFBS_Fn14_Promoter
 
 # Defining SRA ref temp
-SRA_REF='SRR8652105'
+# SRA_REF='SRR8652105'
 
 # Set wd
 set -e
