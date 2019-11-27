d7f10c441f55fae3e9a2152bc079532be0450484
36280641+JoshuaHarris391@users.noreply.github.com <harjo391@gmail.com>
2019-11-28 10:58:51 +1300

    made transfer scripts

---
+++ scripts/transfer_bam.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=transfer_bam_hcs # job name (shows up in the queue)
+#SBATCH --time=48:00:00 #Walltime (HH:MM:SS)
+#SBATCH --mem=1000 # Memory in MB
+#SBATCH --cpus-per-task=1
+#SBATCH --output=slurm_%x_%j.out
+
+# transfering from /scratch to hcs
+DEST=/mnt/hcs/dsm-molecularoncology/Josh_Harris_Bioinformatics/JRA_5/outputs/bam/
+
+rsync -vr /scratch/STUDENT+harjo391/JRA/JRA_5_TFBS_Fn14_Promoter/outputs/alignments/bam $DEST
