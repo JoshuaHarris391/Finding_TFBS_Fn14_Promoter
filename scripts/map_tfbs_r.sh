b798b5190f7b691e015b3da3aeb8bd3c7fc29de1
36280641+JoshuaHarris391@users.noreply.github.com <harjo391@gmail.com>
2019-11-27 19:09:36 +1300

    adjusted resources

--- scripts/map_tfbs_r.sh
+++ scripts/map_tfbs_r.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=map_tfbs_R # job name (shows up in the queue)
 #SBATCH --time=24:00:00 #Walltime (HH:MM:SS)
-#SBATCH --mem=16000 # Memory in MB
-#SBATCH --cpus-per-task=1
+#SBATCH --mem=20000 # Memory in MB
+#SBATCH --cpus-per-task=2
 #SBATCH --output=slurm_%x_%j.out
 
 # Setting wd
