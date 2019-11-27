db5faee19c680693c20ee829ddb4bf19cecd3a6d
36280641+JoshuaHarris391@users.noreply.github.com <harjo391@gmail.com>
2019-11-27 13:47:17 +1300

    script to get wd

--- scripts/r_wd_test.sh
+++ scripts/r_wd_test.sh
@@ -4,3 +4,14 @@
 #SBATCH --mem=1000 # Memory in MB
 #SBATCH --cpus-per-task=1
 #SBATCH --output=slurm_%x_%j.out
+
+# Setting wd
+HOME=/home/STUDENT/harjo391/JRA/JRA_5_TFBS_Fn14_Promoter/
+cd $HOME
+
+# Running Rscript
+module purge
+module load 'R/3.6.0-foss-2019a'
+
+# Running sync
+Rscript r_scripts/r_wd_test.R
