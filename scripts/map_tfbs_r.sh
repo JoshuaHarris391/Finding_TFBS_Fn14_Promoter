021404ba84de5b50977dd9a8036210eb6d34979f
36280641+JoshuaHarris391@users.noreply.github.com <harjo391@gmail.com>
2019-11-27 17:10:15 +1300

    adjusted resource alloc

--- scripts/map_tfbs_r.sh
+++ scripts/map_tfbs_r.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=map_tfbs_R # job name (shows up in the queue)
-#SBATCH --time=2:00:00 #Walltime (HH:MM:SS)
-#SBATCH --mem=1000 # Memory in MB
+#SBATCH --time=24:00:00 #Walltime (HH:MM:SS)
+#SBATCH --mem=8000 # Memory in MB
 #SBATCH --cpus-per-task=1
 #SBATCH --output=slurm_%x_%j.out
 
