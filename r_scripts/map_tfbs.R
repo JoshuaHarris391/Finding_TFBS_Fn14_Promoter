9a2cb099c097837f30d8547b1bdd062c642bf358
36280641+JoshuaHarris391@users.noreply.github.com <harjo391@gmail.com>
2019-11-27 13:23:34 +1300

    first completed version on script

--- r_scripts/map_tfbs.R
+++ r_scripts/map_tfbs.R
@@ -18,20 +18,18 @@
 #' 
 
 
-#' ## Loading and installing Packages
-# if (!requireNamespace("BiocManager", quietly = TRUE))
-#   install.packages("BiocManager")
-# 
-# BiocManager::install("TFBSTools")
+#' #' ## Loading and installing Packages
+#' if (!requireNamespace("BiocManager", quietly = TRUE)){
+#'   install.packages("BiocManager", lib = './R_libs')
+#' }
+#' BiocManager::install(c("TFBSTools", 'JASPAR2018', 'Biostrings', 'seqinr'), lib = './R_libs')
+#' # Installing tidyverse
+#' install.packages('tidyverse', lib = './R_libs')
 
-# if (!requireNamespace("BiocManager", quietly = TRUE))
-#   install.packages("BiocManager")
-# 
-# BiocManager::install("JASPAR2018")
 
-library('TFBSTools')
-library('JASPAR2018')
-library('tidyverse', warn.conflicts = F)
+library('TFBSTools', lib.loc = './R_libs')
+library('JASPAR2018', lib.loc = './R_libs')
+library('tidyverse', warn.conflicts = F, lib.loc = './R_libs')
 
 #' ## Creating JASPAR Library
 suppressMessages(library(JASPAR2018))
@@ -43,49 +41,115 @@ PFMatrixList <- getMatrixSet(JASPAR2018, opts)
 PFMatrixList %>% summary() 
 #' - PFMatrixList contains PFMatrix files for each TF, these will need to be converted to PWM
 
+
+#' # Loading sql query
+JASPAR_Annot_DF <- read.csv(file = "JASPAR_SQL/JASPAR_homo_sapien_TF_annotation.csv", header = T, na.strings = c("", "NA"))
+# Converting BASE_ID to character vector
+JASPAR_Annot_DF$BASE_ID <- JASPAR_Annot_DF$BASE_ID %>% as.character()
+
 #' ## Converting PFM files to PWM
 pwmList <- PFMatrixList %>% toPWM()
 icmList <- PFMatrixList %>% toPWM()
+
 #' # Scanning Sequences for PWM pattern
-library(Biostrings)
-library(seqinr)
+library('Biostrings', lib.loc = './R_libs')
+library('seqinr', lib.loc = './R_libs')
+
 # Loading Reference sequence
 Fn14_pro_refseq <- readDNAStringSet("sequences/Fn14_3kb_up_5utr_hg19_ucsc.fasta")
 Fn14_pro_refseq <- Fn14_pro_refseq %>% paste()
 Fn14_pro_refseq <- DNAString(Fn14_pro_refseq)
 
 # Checking TFBS in reference sequence
-output_ref <- searchSeq(pwmList, Fn14_pro_refseq, seqname="Hg19_Fn14_2kb_upstrem_promoter",
+output_ref <- searchSeq(pwmList, Fn14_pro_refseq, seqname="Hg19_Fn14_3kb_upstrem_promoter",
                         min.score="80%", strand="*")
-output_df_ref <- searchSeq(pwmList, Fn14_pro_refseq, seqname="Hg19_Fn14_2kb_upstrem_promoter",
+output_df_ref <- searchSeq(pwmList, Fn14_pro_refseq, seqname="Hg19_Fn14_3kb_upstrem_promoter",
                            min.score="80%", strand="*") %>% as( "data.frame")
-
-# Making table of IDs
-tfbs_ID_table <- table(output_df_ref$ID)
-tfbs_ID_table <- tfbs_ID_table[unique(output_df_ref$ID)]
-
-input_id <- names(tfbs_ID_table[1])
-t<-PFMatrixList[[input_id]]
-as.list(t)
-PFMatrixList$MA0025.1 %>% as.character()
-attr(PFMatrixList[[1]], "$species")
-
-PFMatrixList@listData
-slot(PFMatrixList[[1]], "species")
-getSlots(PFMatrixList)
-
-# Adding in relative scores
-score_df <- unlist(relScore(output_ref)) %>% as.data.frame()
-output_df_ref$relative_score <- score_df[, 1]
-# Adding in emperical pvalues
-pval_df <- unlist(pvalues(head(output_ref), type = "TFMPvalue")) %>% as.data.frame()
-output_df_ref$pval_emperical <- pval_df[, 1]
 
 
 # Updating chromosmal position details
 output_df_ref$chromosome <- 16
 output_df_ref$start <- output_df_ref$start + 3067313
 output_df_ref$end <- output_df_ref$end + 3067313
+# Function to stich together full genomic region
+genom_reg <- function(start, end, chromosome){
+  x <- paste('chr', chromosome, ':', start, '-', end, sep = '')
+  return(x)
+}
+output_df_ref$location <- genom_reg(output_df_ref$start, output_df_ref$end, output_df_ref$chromosome)
+# Adding distance from ATG
+output_df_ref$dist_atg_start <- 3070398- output_df_ref$start
+output_df_ref$dist_atg_end <- 3070398- output_df_ref$end
+
+
+# Joining JASPAR annotation df with output_df by ID
+output_df_ref$BASE_ID <- gsub('\\.\\d', "", output_df_ref$ID)
+output_df_ref_join <- dplyr::left_join(output_df_ref, JASPAR_Annot_DF, by = "BASE_ID")
+
+# Subsetting columns of intrest
+exclude_cols <- c("source", 'NAME', 'CLASS')
+export_df <- output_df_ref_join[, !(colnames(output_df_ref_join) %in% exclude_cols)]
+# Renaming some columns
+colnames(export_df)[colnames(export_df) == 'ID.x'] <- "FULL_TF_ID"
+colnames(export_df)[colnames(export_df) == 'ID.y']  <- "SAMPLE_ID"
+colnames(export_df)[colnames(export_df) == 'TF']  <- "TF_NAME"
+colnames(export_df)[colnames(export_df) == 'SOURCE']  <- "EXPERIMENTAL_SOURCE"
+# making colnames uppper case
+colnames(export_df) <- colnames(export_df) %>% toupper()
+
+# Reordering Cols
+col_order <- c("SAMPLE_ID",
+               "SEQNAMES",
+               "COLLECTION",
+               "FEATURE",
+               "TAX_ID",
+               "BASE_ID",
+               "FULL_TF_ID",
+               "TF_NAME",
+               "SITESEQS",
+               "CHROMOSOME",
+               "START",
+               "END",
+               "STRAND",
+               "LOCATION",
+               "DIST_ATG_START",
+               "DIST_ATG_END",
+               "FAMILY",
+               "CLASS",
+               "EXPERIMENTAL_SOURCE",
+               "MEDLINE",
+               "TYPE",
+               "DESCRIPTION",
+               "CONSENSUS",
+               "COMMENT",
+               "ABSSCORE",
+               "RELSCORE")
+export_df <- export_df[, col_order]
+
+# Renaming some more columns
+colnames(export_df)[colnames(export_df) == 'ABSSCORE'] <- "ABS_SCORE"
+colnames(export_df)[colnames(export_df) == 'RELSCORE'] <- "REL_SCORE"
+colnames(export_df)[colnames(export_df) == 'SEQNAMES'] <- "SEQ_NAMES"
+colnames(export_df)[colnames(export_df) == 'SITESEQS'] <- "SITE_SEQS"
+
+# Adding in emperical pvalues
+pval_df <- unlist(pvalues(output_ref, type = "TFMPvalue")) %>% as.data.frame()
+export_df$PVAL_EMPERICAL <- pval_df[, 1]
+
+# Adding in adjusted pvalue
+export_df$ADJUST_PVAL <- p.adjust(export_df$PVAL_EMPERICAL, method = "BH", n = length(export_df$PVAL_EMPERICAL))
+
+# Creating Value Filtered DF
+export_df_sig <- export_df[export_df$ADJUST_PVAL < 0.05, ]
+
+# Getting list of unique transcription factors 
+unique_tf <- unique(export_df$TF_NAME)[order(unique(export_df$TF_NAME))]
+unique_tf_sig <- unique(export_df_sig$TF_NAME)[order(unique(export_df_sig$TF_NAME))]
 
 # Exporting dataframe 
-write.table(output_df_ref, file = 'tfbs_table/Fn14_3kb_up_core_tfbs_df.txt', sep = '\t')
+write.csv(export_df, file = 'tfbs_table/Fn14_3kb_up_JASPAR_TFBS.csv')
+write.csv(export_df_sig, file = 'tfbs_table/Fn14_3kb_up_JASPAR_TFBS_SIGNIF.csv')
+
+# Exporting lists
+write(unique_tf, file = "tfbs_table/Fn14_unique_TF.txt")
+write(unique_tf_sig, file = "tfbs_table/Fn14_unique_TF_signif.txt")
