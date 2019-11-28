faf24775e419dbfeb734050672bd6a72ce66fe62
36280641+JoshuaHarris391@users.noreply.github.com <harjo391@gmail.com>
2019-11-28 14:25:59 +1300

    removed array from headder

--- scripts/array_test.sh
+++ scripts/array_test.sh
@@ -3,8 +3,7 @@
 #SBATCH --time=1:00:00 #Walltime (HH:MM:SS)
 #SBATCH --mem=1000 # Memory in MB
 #SBATCH --cpus-per-task=1
-#SBATCH --output=array_%A_%a.out
-#SBATCH --array=1-4
+#SBATCH --output=slurm_%x_%j.out
 
 
 cmd="sbatch --array=1-4 scripts/array_test_slave_echo.sh"
