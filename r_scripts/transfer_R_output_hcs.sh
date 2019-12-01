fa0fc898e4ac64c7d616779c57bea5aec2e86f95
36280641+JoshuaHarris391@users.noreply.github.com <harjo391@gmail.com>
2019-12-01 21:01:12 +1300

    transfers R outputs to hcs

---
+++ r_scripts/transfer_R_output_hcs.sh
@@ -0,0 +1,3 @@
+# Transferring tfbs R stuff to hcs
+rsync -vr /Users/joshua_harris/Dropbox/Research/JRA_Cunliffe_Lab_2019/Experiments/JRA_5/JRA_5_TFBS_Fn14_Promoter/tfbs_table \
+					/Volumes/dsm-molecularoncology/Josh_Harris_Bioinformatics/JRA_5/outputs/
