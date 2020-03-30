f4bf2bfeca4c56f2be2b3757f1e3e7c05e24f572
36280641+JoshuaHarris391@users.noreply.github.com <harjo391@gmail.com>
2020-03-30 14:46:32 +1300

    added script to clear scratch folder

---
+++ scripts/backup_scratch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=transfer_JRA5_HCS # job name (shows up in the queue)
+#SBATCH --time=24:00:00 #Walltime (HH:MM:SS)
+#SBATCH --mem=1000 # Memory in MB
+#SBATCH --cpus-per-task=1
+#SBATCH --output=slurm_%x_%j.out
+
+# transfering from /scratch to hcs
+rsync -vr /scratch/STUDENT+harjo391/JRA_5 /mnt/hcs/dsm-molecularoncology/Josh_Harris_Bioinformatics/
+# Removing folder from scratch
+rm -R /scratch/STUDENT+harjo391/JRA_5
